0cfb349048c89b328c582a19359c149f4a9a1f52
Eunmo Yeo <emyeo@marusys.com>
2013-10-16 21:01:20 +0900

    Separate xbmc and M/W service.

--- recipes-mediacentre/xbmc/xbmc/elmo/run.xbmc.sh
+++ recipes-mediacentre/xbmc/xbmc/elmo/run.xbmc.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+export EDITOR='/bin/vi'
+export HOME='/home/root'
+export LOGNAME='root'
+export OPIEDIR
+export PATH='/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin'
+export PS1='\u@\h:\w\$ '
+export PWD='/home/root'
+export QPEDIR
+export QTDIR
+export SHELL='/bin/sh'
+export TERM='linux'
+export TSLIB_TSDEVICE='/dev/input/touchscreen0'
+export TZ='UTC'
+export USER='root'
+export LD_LIBRARY_PATH=/opt/lib
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/lib
 
 while true
